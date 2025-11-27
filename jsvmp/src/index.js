d80f9d329d24f595b29bcc15c7488dbc1b250b33
0xfffb <jailbreak@aliyun.com>
2025-11-27 10:33:17 +0800

    style: reanmed

--- jsvmp/src/index.js
+++ jsvmp/src/index.js
@@ -1,5 +1,5 @@
 import VM from './vm/vm.js'
-import { toDecimalBytecode } from './utils/bytecode.js'
+import { decimalToBytecode } from './utils/bytecode.js'
 
 (function main() {
     const vm = new VM()
@@ -22,7 +22,7 @@ import { toDecimalBytecode } from './utils/bytecode.js'
     const code = [ 0, 10, 0, 20, 2 ]
     const hexBytecode = [ '0x00', '0x0a', '0x00', '0x14', '0x02' ]
 
-    const bytecode = toDecimalBytecode(hexBytecode)
+    const bytecode = decimalToBytecode(hexBytecode)
     const result = vm.execute(bytecode)
     console.log(result)
 })()
--- jsvmp/src/utils/bytecode.js
+++ jsvmp/src/utils/bytecode.js
@@ -1,11 +1,11 @@
 
 
-function toDecimalBytecode(code) {
+function decimalToBytecode(code) {
     return code.map(c => parseInt(c, 16))
 }
 
-function toHexBytecode(code) {
+function hexToBytecode(code) {
     return code.map(c => '0x' + c.toString(16).padStart(2, '0'))
 }
 
-export { toDecimalBytecode, toHexBytecode }
+export { decimalToBytecode, hexToBytecode }
