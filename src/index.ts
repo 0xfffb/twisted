993f362569aafba4e724d7ee9f5362c62386d71a
0xfffb <jailbreak@aliyun.com>
2025-12-05 10:39:52 +0800

    style: format

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import Compiler from './compiler/compiler.js';
-import { OPCODE } from './constant.js';
+import Compiler from "./compiler/compiler.js";
+import { OPCODE } from "./constant.js";
 
 function main() {
-    const code = `
+	const code = `
 let a = 10 + 20
 console.log(a)
     `;
-    const compiler = new Compiler(code, OPCODE);
-    const ir = compiler.compile();
-    console.dir(ir, { depth: null });
+	const compiler = new Compiler(code, OPCODE);
+	const ir = compiler.compile();
+	console.dir(ir, { depth: null });
 }
 
 main();
--- src/obfuscator/base.js
+++ src/obfuscator/base.js
@@ -1,39 +1,39 @@
-import { parse } from '@babel/parser';
-import { generate as _generate } from '@babel/generator';
+import { parse } from "@babel/parser";
+import { generate as _generate } from "@babel/generator";
 
 class AbstractObfuscator {
-    constructor(code, transformers) {
-        this.code = code;
-        this.transformers = transformers;
-        this.ast = this.parse();
-    }
+	constructor(code, transformers) {
+		this.code = code;
+		this.transformers = transformers;
+		this.ast = this.parse();
+	}
 
-    obfuscate() {
-        throw new Error('Obfuscator must implement obfuscate method');
-    }
+	obfuscate() {
+		throw new Error("Obfuscator must implement obfuscate method");
+	}
 
-    parse() {
-        try {
-            const ast = parse(this.code, {
-                sourceType: 'module',
-                plugins: ['jsx', 'typescript'],
-            });
-            return ast;
-        } catch (error) {
-            console.error(`Error parsing code: ${error}`);
-            process.exit(1);
-        }
-    }
+	parse() {
+		try {
+			const ast = parse(this.code, {
+				sourceType: "module",
+				plugins: ["jsx", "typescript"],
+			});
+			return ast;
+		} catch (error) {
+			console.error(`Error parsing code: ${error}`);
+			process.exit(1);
+		}
+	}
 
-    generate() {
-        try {
-            const result = _generate(this.ast, {});
-            return result.code;
-        } catch (error) {
-            console.error(`Error generating code: ${error}`);
-            process.exit(1);
-        }
-    }
+	generate() {
+		try {
+			const result = _generate(this.ast, {});
+			return result.code;
+		} catch (error) {
+			console.error(`Error generating code: ${error}`);
+			process.exit(1);
+		}
+	}
 }
 
 export default AbstractObfuscator;
