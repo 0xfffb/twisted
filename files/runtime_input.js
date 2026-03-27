165fbff389d2de079f19126a6f04ca35a7bdc43f
0xfffb <jailbreak@aliyun.com>
2026-03-27 15:08:01 +0800

    feat: fingerprint

--- files/runtime_input.js
+++ files/runtime_input.js
@@ -1,6 +1,21 @@
-const time = window.Date();
-const bytes = new window.TextEncoder().encode(time);
+const canvas = window.document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillText("twisted-fp", 2, 15);
+const canvasFp = canvas.toDataURL();
+
+const fingerprint = {
+	ua: window.navigator.userAgent,
+	lang: window.navigator.language,
+	platform: window.navigator.platform,
+	hc: window.navigator.hardwareConcurrency,
+	tz: window.Intl.DateTimeFormat().resolvedOptions().timeZone,
+	canvas: canvasFp,
+};
+
+const payload = window.JSON.stringify(fingerprint);
+const bytes = new window.TextEncoder().encode(payload);
 const digest = await window.crypto.subtle.digest("SHA-256", bytes);
-const digestBytes = new window.Uint8Array(digest);
-const fp32 = "0x" + new window.DataView(digest).getUint32(0, false).toString(16);
-fp32
+const hash = new window.DataView(digest).getUint32(0, false).toString(16);
+
+const result = { fingerprint: fingerprint, hash: "0x" + hash,  };
+window.JSON.stringify(result)
