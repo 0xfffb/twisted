78c181177f52724965fdf28ba7d50f021f9676e4
0xfffb <jailbreak@aliyun.com>
2026-04-15 10:26:07 +0800

    feat: test

--- src/compiler/hyperion.ts
+++ src/compiler/hyperion.ts
@@ -1,14 +1,54 @@
+import * as parser from "@babel/parser";
 import { BaseCompiler } from "./base.js";
+import { IRBuilder } from "./builder.js";
+import { IRModule } from "./module.js";
+import { Expression, Identifier, MemberExpression, Program, Statement } from "@babel/types";
 
 
 class HyperionCompiler extends BaseCompiler {
+    
+    readonly builder: IRBuilder;
+    private program: Program;
+
 	constructor(source: string) {
 		super(source);
+        this.builder = new IRBuilder(new IRModule("hyperion"));
+        this.program = parser.parse(source, { sourceType: "module" }).program;
 	}
 
-	compile(): any[] {
-		throw new Error("Not implemented");
+	compile(): any {
+        const module = this.builder.module;
+        this.program.body.forEach((element) => {
+			this.compileStatement(element as Statement);
+		});
+        return module;
 	}
+
+    private compileStatement(node: Statement) {
+        switch (node.type) {
+            case "ExpressionStatement":
+                this.compileExpression(node.expression);
+                break;
+        }
+    }
+
+    private compileExpression(node: Expression) {
+        switch (node.type) {
+            case "Identifier":
+                this.compileIdentifier(node as Identifier);
+                break;
+            case "MemberExpression":
+                this.compileMemberExpression(node as MemberExpression);
+                break;
+        }
+    }
+
+    private compileIdentifier(node: Identifier) {
+        
+    }
+
+    private compileMemberExpression(node: MemberExpression) {
+    }
 }
 
 export { HyperionCompiler };
--- src/debugger.ts
+++ src/debugger.ts
@@ -1,52 +1,13 @@
-import { LinearCompiler } from "./compiler/index.js";
-import Assembler from "./assembler/assembler.js";
-import VM from "./vm/vm.js";
-import { JSDOM } from "jsdom";
-import { Instruction } from "./instruction.js";
-import { OPCODE_NAMES } from "./constant.js";
-import { ArithmeticDeformationPass } from "./obfuscator/passes/arithmetic.js";
-import Obfuscator from "./obfuscator/obfuscator.js";
+import { HyperionCompiler } from "./compiler/index.js";
 
-function debugInstruction(ir: Instruction[]) {
-	ir.forEach((instruction) => {
-		console.log({ opcode: OPCODE_NAMES[instruction.opcode], args: instruction.args });
-	});
-}
 
 async function main() {
 	const code = `
-// console.log(window.tws)
-// if (window.tws) {
-// 	console.log("find tws")
-// }
-const a = 1;
-const b = 2;
-const c = a + b;
-console.log(c);
-
-if (!window.tws) { console.log("hit"); } else { console.log("miss"); }
+var a = 1 + 2 + 3;
     `;
-	const compiler = new LinearCompiler(code);
-	const ir = compiler.compile();
-
-	const passes = [new ArithmeticDeformationPass()];
-	const obfuscator = new Obfuscator(passes);
-
-	const obfuscatedIr = obfuscator.obfuscate(ir);
-	const assembler = new Assembler();
-	const bundle = assembler.assemble(obfuscatedIr);
-	console.log("// --- 编译后 IR（未混淆）---");
-	debugInstruction(ir);
-	console.log("// --- ArithmeticDeformationPass 之后 ---");
-	debugInstruction(obfuscatedIr);
-	console.dir(bundle.meta, { depth: null });
-	console.dir(bundle.bytecode, { depth: null });
-	console.log("IR length:", obfuscatedIr.length);
-	const dom = new JSDOM();
-	const dependencies = [dom.window, dom.window.console];
-	const vm = new VM(bundle.bytecode, bundle.meta, dependencies);
-	const result = await vm.execute();
-	console.log(result);
+	const compiler = new HyperionCompiler(code);
+	const module = compiler.compile();
+	console.dir(module, { depth: null });
 }
 
 void main();
